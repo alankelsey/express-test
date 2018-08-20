ce4393e94a0a9ead0b42200f10277fbffebd326a
Alan Kelsey <akelsey@Alans-MacBook-Pro.local>
2018-08-20 08:19:00 -0600

    relead preventing azure container from starting app- removed reference for now

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const errorhandler_1 = __importDefault(require("errorhandler"));
 const app_1 = __importDefault(require("./app"));
-const reload = require("reload");
+
 /**
  * Error Handler. Provides full stack - remove for production
  */
@@ -17,6 +17,5 @@ const server = app_1.default.listen(app_1.default.get("port"), () => {
     console.log("  App is running at http://localhost:%d in %s mode", app_1.default.get("port"), app_1.default.get("env"));
     console.log("  Press CTRL-C to stop\n");
 });
-reload(server, app_1.default);
 exports.default = server;
 //# sourceMappingURL=server.js.map
